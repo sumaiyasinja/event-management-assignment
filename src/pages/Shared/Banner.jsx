df0265bf5792730a780f81f3313985d14f66ea3a
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-10-07 21:39:40 +0600

    Banner section text position changed

--- src/pages/Shared/Banner.jsx
+++ src/pages/Shared/Banner.jsx
@@ -18,7 +18,7 @@ const Banner = () => {
     <div className="relative">
       <img src="https://i.ibb.co/d6VHtdF/banner.jpg" alt="Banner Image"
        className="w-full bg-cover opacity-70" />
-      <p className="absolute top-[45%] left-0 right-0 text-5xl bannertext text-white text-center py-4 bg-opacity-70 bg-transparent">
+      <p className="absolute top-[30%] left-0 right-0 text-5xl bannertext text-white text-center py-4 bg-opacity-70 bg-transparent">
       <span className='font-black text-[180px] text-white navlogo'>{bannerTexts[textIndex]}</span> <br /> <br />
         <span className='text-gray-700 w-full'>Let Us Plan Your Next Event Together..</span>
       </p>
