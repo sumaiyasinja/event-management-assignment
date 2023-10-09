7f49fec3777fbeb6abb7b1ad25227c2c39022f26
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-10-09 20:57:43 +0600

    Respionsivce navbar bg color changed

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -33,16 +33,18 @@ const NavLinks= <>
     <div className="navbar bg-[#D6A2E8] rounded-lg font-medium text-white text-2xl">
         <div className="navbar-start">
             <div className="dropdown">
+                {/* mobile */}
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content bg-lavender text-black mt-3 z-[1] p-2 shadow rounded-box w-52">
                     {NavLinks}
                 </ul>
             </div>
         </div>
+        {/* large */}
         <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
                 {NavLinks}
