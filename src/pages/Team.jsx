1f92b3367439b7b36104cd4c4abd43659fd6e270
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-10-10 17:33:02 +0600

    team page design improved

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -17,16 +17,15 @@ const Team = () => {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <div 
                  data-aos="fade-up" 
-                 data-aos-duration="800"
-                className="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="relative">
-                        <img
-                            src="https://i.ibb.co/6HQ37Kh/stefan-stefancik-QXev-Dflbl8-A-unsplash.jpg"
-                            alt="Sumaiya Sinja"
-                            className="w-full h-56 object-cover object-center"
-                        />
+                 data-aos-duration="800"className="rounded-xl overflow-hidden h-96 flex items-center justify-center">
+                 <div className="relative hover:border-8 border-lavender rounded-full w-[270px] flex items-center justify-center">
+                     <img
+                  src="https://i.ibb.co/1Q7Kbg9/business-concept-portrait-confident-young-businesswoman-keeping-arms-crossed-looking-camera-w.jpg"
+                         alt="Mr. X"
+                         className="w-64 h-64 object-cover object-center rounded-full p-3"
+                     />
                     </div>
-                    <div className="py-4 px-6">
+                    <div className="py-4 px-6 flex justify-center items-center flex-col">
                         <h2 className="text-2xl font-semibold text-gray-800">Sumaiya Sinja</h2>
                         <p className="text-gray-600">CEO and Founder</p>
                     </div>
@@ -35,36 +34,39 @@ const Team = () => {
                 <div 
                    data-aos="fade-up" 
                    data-aos-duration="800"
-                   className="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="relative">
+                   
+                   className="rounded-xl overflow-hidden h-96 flex felx-col md:flex-row items-center justify-center">
+                   <div className="relative hover:border-8 border-lavender rounded-full w-[270px] flex items-center justify-center">
                         <img
-                            src="https://i.ibb.co/QnF7Lv6/370162076-1353948895532758-114744159248260084-n.jpg"
+                     src="https://i.ibb.co/Krv0XWQ/1-qwh-Wmyn-N8ndgc7o-AQQz-WKA.webp"
                             alt="Mr. X"
-                            className="w-full h-56 object-cover object-center"
+                            className="w-64 h-64 object-cover object-center rounded-full p-3"
                         />
                     </div>
-                    <div className="py-4 px-6">
+                    <div className="py-4 px-6 flex justify-center items-center flex-col">
+                        <h2 className="text-2xl font-semibold text-gray-800">Mr. X</h2>
+                        <p className="text-gray-600">Founder & Team Manager</p>
+                    </div>
+                </div>
+                <div 
+                   data-aos="fade-up" 
+                   data-aos-duration="800"
+                   
+                   className="rounded-xl overflow-hidden h-96 flex items-center justify-center">
+                   <div className="relative hover:border-8 border-lavender rounded-full w-[270px] flex items-center justify-center">
+                        <img
+                     src="https://i.ibb.co/QnF7Lv6/370162076-1353948895532758-114744159248260084-n.jpg"
+                            alt="Mr. X"
+                            className="w-64 h-64 object-cover object-center rounded-full p-3"
+                        />
+                    </div>
+                    <div className="py-4 px-6 flex justify-center items-center flex-col">
                         <h2 className="text-2xl font-semibold text-gray-800">Mr. X</h2>
                         <p className="text-gray-600">Founder & Team Manager</p>
                     </div>
                 </div>
 
-                <div
-                   data-aos="fade-up" 
-                   data-aos-duration="800"
-                    className="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <div className="relative">
-                        <img
-                            src="https://i.ibb.co/dQF58hy/384862078-3596194700650498-1752904254446908345-n.jpg"
-                            alt="Ms. Ana"
-                            className="w-full h-56 object-cover object-center"
-                        />
-                    </div>
-                    <div className="py-4 px-6">
-                        <h2 className="text-2xl font-semibold text-gray-800">Ms. Ana</h2>
-                        <p className="text-gray-600">Chef</p>
-                    </div>
-                </div>
+
             </div>
         </div>
     );
